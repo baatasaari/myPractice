de99db88c2c623770fe1dddee1d649a347bafaf1
Vijay <44259548+baatasaari@users.noreply.github.com>
2018-10-18 15:33:24 +0100

    post static analysis

--- Hello.py
+++ Hello.py
@@ -4,18 +4,18 @@ Spyder Editor
 
 This is a temporary script file.
 """
-print ("hello world")
-str = 'Hello World!'
-print (str)
-print (str)          # Prints complete string
-print (str[0])       # Prints first character of the string
-print (str[2:5])     # Prints characters starting from 3rd to 5th
-print (str[2:])      # Prints string starting from 3rd character
-print (str * 2)      # Prints string two times
-print (str + "TEST") # Prints concatenated string
-counter = 100          # An integer assignment
-miles   = 1000.0       # A floating point
-name    = "John"       # A string
-print (counter)
-print (miles)
-print (name)
+print("hello world")
+STR = 'Hello World!'
+print(STR)
+print(STR)          # Prints complete string
+print(STR[0])       # Prints first character of the string
+print(STR[2:5])     # Prints characters starting from 3rd to 5th
+print(STR[2:])      # Prints string starting from 3rd character
+print(STR * 2)      # Prints string two times
+print(STR + "TEST") # Prints concatenated string
+COUNTER = 100          # An integer assignment
+MILES = 1000.0       # A floating point
+NAME = "John"       # A string
+print(COUNTER)
+print(MILES)
+print(NAME)
